9030ebfb26b91dd56d3d0dd141dcd1f0eade627c
Soumen Karmakar <soumen.karmakar@ericsson.com>
2020-05-13 14:22:44 +0100

    Adding Security

--- web/src/main/java/org/jake/library/security/WebSecurityConfig.java
+++ web/src/main/java/org/jake/library/security/WebSecurityConfig.java
@@ -1,45 +1,41 @@
 package org.jake.library.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import static org.jake.library.security.UserDetailsServiceImpl.*;
+
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-        auth.inMemoryAuthentication()
-                .withUser("patron")
-                .password("123")
-                .roles("PATRON")
-                .and()
-                .withUser("admin")
-                .password("123")
-                .roles("ADMIN")
-                .and()
-                .withUser("librarian")
-                .password("123")
-                .roles("LIBRARIAN");
+        auth.userDetailsService(this.userDetailsService);
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+
+        http.csrf().disable().authorizeRequests()
                 .antMatchers("/").permitAll()
-                .antMatchers("/patronServices", "/bookSearch", "/search").hasAnyRole("PATRON")
-                .antMatchers("/librarianServices", "/managePatrons").hasAnyRole("LIBRARIAN")
-                .antMatchers("/adminServices").hasAnyRole("ADMIN")
+                .antMatchers("/patronServices", "/bookSearch", "/search").hasAnyRole(ROLE_PATRON, ROLE_ADMIN)
+                .antMatchers("/librarianServices", "/managePatrons").hasAnyRole(ROLE_LIBRARIAN, ROLE_ADMIN)
+                .antMatchers("/adminServices").hasAnyRole(ROLE_ADMIN)
                 .and().formLogin();
     }
 
     @Bean
-    public PasswordEncoder getPasswordEncoder(){
+    public PasswordEncoder getPasswordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
 }
